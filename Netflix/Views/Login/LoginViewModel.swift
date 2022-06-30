addfaea1cc259a9fa57319128564934775f36a19
Marek Roslik <marek.roslik@gmail.com>
2022-06-30 15:58:03 +0300

    Complete login in MVVM

--- Netflix/Views/Login/LoginViewModel.swift
+++ Netflix/Views/Login/LoginViewModel.swift
@@ -32,9 +32,8 @@ final class LoginViewModel: ViewModelType {
             .withLatestFrom(Observable.combineLatest(input.login, input.password))
             .map { [ weak self] (login, password) in
                 guard let self = self else { return }
-                self.getToken(bag: self.bag)
                 self.authenticationWithLoginPassword(login: login, password: password, bag: self.bag)
-        }.asDriver(onErrorJustReturn: ())
+            }.asDriver(onErrorJustReturn: ())
         
         let inputValidating = Observable.combineLatest(input.login.startWith(""), input.password.startWith("")) { login, password in
             return login.count >= 1  && password.count >= 4
@@ -51,58 +50,53 @@ final class LoginViewModel: ViewModelType {
             accessDenied: accessDenied)
     }
     
-    func getToken(bag: DisposeBag) {
-            apiClient.getToken().subscribe(
-                onNext: { result in
-                    UserDefaultsUseCase().token = result.requestToken
-                },
-                onError: { [weak self] _ in
-                    self?.errorHandling.onNext("Login failed. Please try again later")
-                }).disposed(by: bag)
-        }
-    
     func authenticationWithLoginPassword(
-            login: String,
-            password: String,
-            bag: DisposeBag) {
-            let loginPost = LoginPostResponseModel(
-                username: login,
-                password: password,
-                requestToken: UserDefaultsUseCase().token!)
+        login: String,
+        password: String,
+        bag: DisposeBag) {
+                let loginPost = LoginPostResponseModel(
+                    username: login,
+                    password: password,
+                    requestToken: UserDefaultsUseCase().token ?? "")
                 apiClient.authenticationWithLoginPassword(model: loginPost )
-                    .observe(on: MainScheduler.instance)
                     .subscribe(onNext: { [weak self] _ in
-                    self?.saveKeyChain(login: login, password: password)
-                    self?.getSessionId(token: UserDefaultsUseCase().token!, bag: bag)
-                },
-                onError: { [weak self] error in
-                    switch error {
-                    case APIError.wrongPassword:
-                        self?.errorHandling.onNext("Invalid username or password")
-                    default:
-                        self?.errorHandling.onNext("Login failed. Please try again later")
-                    }
-                }).disposed(by: bag)
+                        guard let self = self else { return }
+                        self.saveKeyChain(login: login, password: password)
+                    },
+                               onError: { [weak self] error in
+                        guard let self = self else { return }
+                        switch error {
+                        case APIError.wrongPassword:
+                            self.errorHandling.onNext("Invalid username or password")
+                        default:
+                            self.errorHandling.onNext("Login failed. Please try again later")
+                        }
+                    }, onCompleted: { [weak self] in
+                        guard let self = self else { return }
+                        self.getSessionId(bag: self.bag)
+                    }).disposed(by: bag)
         }
     
-    func getSessionId(token: String, bag: DisposeBag) {
-            let sessionIdPost = SessionIdPostResponseModel(token: token)
-            apiClient.getSessionId(model: sessionIdPost)
-                .observe(on: MainScheduler.instance)
-                .subscribe(onNext: { value in
-                    UserDefaultsUseCase().sessionId = value.sessionID
-                    self.didSendEventClosure?(.main)
-                },
-                           onError: { [weak self] _ in
-                    self?.errorHandling.onNext("Login failed. Please try again later")
-                }).disposed(by: bag)
-        }
+    func getSessionId(bag: DisposeBag) {
+        let sessionIdPost = SessionIdPostResponseModel(token: UserDefaultsUseCase().token ?? "" )
+        apiClient.getSessionId(model: sessionIdPost)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { value in
+                UserDefaultsUseCase().sessionId = value.sessionID
+            },
+                       onError: { [weak self] _ in
+                self?.errorHandling.onNext("Login failed. Please try again later")
+            }, onCompleted: { [weak self] in
+                guard let self = self else { return }
+                self.didSendEventClosure?(.main)
+            }).disposed(by: bag)
+    }
     
     func saveKeyChain(login: String, password: String) {
-            do {
-                try KeyChainUseCase().saveLoginAndPassword(login: login, password: password.data(using: .utf8)!)
-            } catch {
-                print("KEYCHAIN SAVE \(error)")
-            }
+        do {
+            try KeyChainUseCase().saveLoginAndPassword(login: login, password: password.data(using: .utf8)!)
+        } catch {
+            print("KEYCHAIN SAVE \(error)")
         }
+    }
 }
