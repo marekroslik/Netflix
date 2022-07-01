9492ae10772619c9fc68604a1fdc6e673d80307d
Marek Roslik <marek.roslik@gmail.com>
2022-07-01 16:21:02 +0300

    Try in true rx

--- Netflix/Views/Login/LoginViewModel.swift
+++ Netflix/Views/Login/LoginViewModel.swift
@@ -28,68 +28,58 @@ final class LoginViewModel: ViewModelType {
     }
     
     func transform(input: Input) -> Output {
-        let accessCheck = input.loginTrigger
-            .withLatestFrom(Observable.combineLatest(input.login, input.password))
-            .map { [ weak self] (login, password) in
-                guard let self = self else { return }
-                self.authenticationWithLoginPassword(login: login, password: password, bag: self.bag)
-            }.asDriver(onErrorJustReturn: ())
         
-        let inputValidating = Observable.combineLatest(input.login.startWith(""), input.password.startWith("")) { login, password in
+        let inputValidating = Observable.combineLatest(
+            input.login.startWith(""),
+            input.password.startWith("")
+        ) { login, password in
             return login.count >= 1  && password.count >= 4
-        }.startWith(false).asDriver(onErrorJustReturn: false)
+        }
+            .startWith(false)
+            .asDriver(onErrorJustReturn: false)
         
-        let showHidePassword = input.showHidePasswordTrigger.asDriver(onErrorJustReturn: ())
+        let accessCheck = input.loginTrigger
+            .withLatestFrom(inputValidating)
+            .filter { $0 }
+            .flatMapLatest { [apiClient] _ in
+                apiClient.getToken()
+            }
+            .flatMapLatest { tokenResponse in
+                UserDefaultsUseCase().token = tokenResponse.requestToken
+            }
+            .withLatestFrom(Observable.combineLatest(
+                input.login.startWith(""),
+                input.password.startWith("")
+            ))
+            .flatMapLatest({ [apiClient] (login, password) in
+                let model = LoginPostResponseModel(
+                    username: login,
+                    password: password,
+                    requestToken: UserDefaultsUseCase().token)
+                apiClient.authenticationWithLoginPassword(model: model)
+            })
+            .flatMapLatest({ [apiClient] _ in
+                let model = SessionIdPostResponseModel(
+                    token: UserDefaultsUseCase().token)
+                apiClient.getSessionId(model: SessionIdPostResponseModel)
+            })
+            .flatMapLatest({ sessionIDResponse in
+                UserDefaultsUseCase().sessionId = sessionIDResponse.sessionID
+                self.didSendEventClosure?(.main)
+            })
+            .asDriver(onErrorJustReturn: ())
         
-        let accessDenied = errorHandling.asDriver(onErrorJustReturn: "")
+        let showHidePassword = input.showHidePasswordTrigger
+            .asDriver(onErrorJustReturn: ())
+        
+        let accessDenied = errorHandling
+            .asDriver(onErrorJustReturn: "")
         
         return Output(
             inputValidating: inputValidating,
             accessCheck: accessCheck,
             showHidePassword: showHidePassword,
             accessDenied: accessDenied)
-    }
-    
-    func authenticationWithLoginPassword(
-        login: String,
-        password: String,
-        bag: DisposeBag) {
-                let loginPost = LoginPostResponseModel(
-                    username: login,
-                    password: password,
-                    requestToken: UserDefaultsUseCase().token ?? "")
-                apiClient.authenticationWithLoginPassword(model: loginPost )
-                    .subscribe(onNext: { [weak self] _ in
-                        guard let self = self else { return }
-                        self.saveKeyChain(login: login, password: password)
-                    },
-                               onError: { [weak self] error in
-                        guard let self = self else { return }
-                        switch error {
-                        case APIError.wrongPassword:
-                            self.errorHandling.onNext("Invalid username or password")
-                        default:
-                            self.errorHandling.onNext("Login failed. Please try again later")
-                        }
-                    }, onCompleted: { [weak self] in
-                        guard let self = self else { return }
-                        self.getSessionId(bag: self.bag)
-                    }).disposed(by: bag)
-        }
-    
-    func getSessionId(bag: DisposeBag) {
-        let sessionIdPost = SessionIdPostResponseModel(token: UserDefaultsUseCase().token ?? "" )
-        apiClient.getSessionId(model: sessionIdPost)
-            .observe(on: MainScheduler.instance)
-            .subscribe(onNext: { value in
-                UserDefaultsUseCase().sessionId = value.sessionID
-            },
-                       onError: { [weak self] _ in
-                self?.errorHandling.onNext("Login failed. Please try again later")
-            }, onCompleted: { [weak self] in
-                guard let self = self else { return }
-                self.didSendEventClosure?(.main)
-            }).disposed(by: bag)
     }
     
     func saveKeyChain(login: String, password: String) {
